b6bedb18f4025418f1483b83944d3b78e578a1da
MasonB <mbbeckmeyer9@gmail.com>
2023-02-17 09:55:42 -0800

    DOne with users and posts class

--- project_src/php/Posts.php
+++ project_src/php/Posts.php
@@ -1,7 +1,29 @@
 <?php
 class Posts{
     private $user;
-    
+    private $postText;
+    private $postPicURL = null;
+    private $dateOfPost;
+
+    function __construct($user,$postText,$postPicURL){
+        $this->user = $user;
+        $this->postText = $postText;
+        $this->postPicURL = $postPicURL;
+        $this->dateOfPost = date_create();
+    }
+
+    function get_user(){
+        return $this->user;
+    }
+    function get_postText(){
+        return $this->postText;
+    }
+    function get_postPicURL(){
+        return $this->postPicURL;
+    }
+    function get_date(){
+        return $this->dateOfPost;
+    }
 }
 
 
