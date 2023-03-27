d8b0acdfec5b46e98ef37300cc100990dd99ba0d
slawal5956 <slawal12345@gmail.com>
2023-03-27 18:34:17 -0400

    calendar changed but not working

--- project_src/html/calendar.php
+++ project_src/html/calendar.php
@@ -1,3 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>Hooked</title>
+<link href="Hooked.css" rel="stylesheet">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Welcome to Hooked">
+</head>
+
+<body>
+
+<header>
+	<!-- <h1><a href="index.html"> H∞ked
+								</a></h1> -->
+<div id="logo"></div>
+</header>
+
+<nav>
+    
+	<ul>
+
+        
+		
+
+        
+		<li>
+            <a href="index.php">Home</a></li>
+
+	
+		<li><a href="calendar.php">Calendar</a></li>
+		<li><a href="login.php">Feed</a></li>
+		<li><a href="contact.php">Messaging</a></li>
+		<li><a href="design.php">Account</a></li>
+        
+        
+	</ul>
+
+
+			
+
+	
+
+</nav>
+<main>
+<header>
+
+
 <?php
 class Calendar {
 
