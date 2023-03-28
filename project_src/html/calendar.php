bace89c8101602549f7446014a4ddde08af560f8
slawal5956 <slawal12345@gmail.com>
2023-03-28 11:08:56 -0400

    calendar changed to add events need to fix css

--- project_src/html/calendar.php
+++ project_src/html/calendar.php
@@ -1,101 +1,27 @@
 <?php
-// Set timezone
-date_default_timezone_set('UTC');
-
-// Get current month and year
-$month = isset($_GET['month']) ? intval($_GET['month']) : date('n');
-$year = isset($_GET['year']) ? intval($_GET['year']) : date('Y');
-
-// Get number of days in the month
-$num_days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-
-// Get the name of the month
-$month_name = date('F', mktime(0, 0, 0, $month, 1, $year));
+include 'classCalendar.php';
+$calendar = new Calendar('2021-02-02');
+$calendar->add_event('Birthday', '2021-02-03', 1, 'green');
+$calendar->add_event('Doctors', '2021-02-04', 1, 'red');
+$calendar->add_event('Holiday', '2021-02-16', 7);
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Calendar</title>
-	<style>
-		.calendar {
-			display: flex;
-			flex-flow: column;
-		}
-		.calendar .header .month-year {
-			font-size: 20px;
-			font-weight: bold;
-			color: #636e73;
-			padding: 20px 0;
-		}
-		.calendar .days {
-			display: flex;
-			flex-flow: wrap;
-		}
-		.calendar .days .day_name {
-			width: calc(100% / 7);
-			border-right: 1px solid #2c7aca;
-			padding: 20px;
-			text-transform: uppercase;
-			font-size: 12px;
-			font-weight: bold;
-			color: #818589;
-			color: #fff;
-			background-color: #448cd6;
-		}
-		.calendar .days .day_name:nth-child(7) {
-			border: none;
-		}
-		.calendar .days .day_num {
-			display: flex;
-			flex-flow: column;
-			width: calc(100% / 7);
-			border-right: 1px solid #e6e9ea;
-			border-bottom: 1px solid #e6e9ea;
-			padding: 15px;
-			font-weight: bold;
-			color: #7c878d;
-			cursor: pointer;
-			min-height: 100px;
-		}
-		.calendar .days .day_num span {
-			display: inline-flex;
-			width: 30px;
-			font-size: 14px;
-		}
-		.calendar .days .day_num .event {
-			margin-top: 10px;
-			font-weight: 500;
-			font-size: 14px;
-			padding: 3px 6px;
-			border-radius: 4px;
-			background-color: #f7c30d;
-			color: #fff;
-			word-wrap: break-word;
-		}
-		.calendar .days .day_num .event.green {
-			background-color: #51ce57;
-		}
-		.calendar .days .day_num .event.blue {
-			background-color: #518fce;
-		}
-		.calendar .days .day_num .event.red {
-			background-color: #ce5151;
-		}
-		.calendar .days .day_num:nth-child(7n+1) {
-			border-left: 1px solid #e6e9ea;
-		}
-		.calendar .days .day_num:hover {
-			background-color: #fdfdfd;
-		}
-		.calendar .days .day_num.ignore {
-			background-color: #fdfdfd;
-			color: #ced2d4;
-			cursor: inherit;
-		}
-		.calendar .days .day_num.selected {
-			background-color: #f1f2f3;
-			cursor: inherit;
-		}
-	</style>
-</
+	<head>
+		<meta charset="utf-8">
+		<title>Event Calendar</title>
+		<link href="style.css" rel="stylesheet">
+		<link href="calendar.css" rel="stylesheet">
+  
+	</head>
+	<body>
+	    <nav class="navtop">
+	    	<div>
+	    		<h1>Event Calendar</h1>
+	    	</div>
+	    </nav>
+		<div class="content home">
+			<?=$calendar?>
+		</div>
+	</body>
+</html>
